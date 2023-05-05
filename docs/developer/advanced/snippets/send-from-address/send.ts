a25d59622e0a4a530c60777599cfc65921c2bb90
Rob De Feo <robdefeo@gmail.com>
2023-05-05 10:33:34 +0200

    fix: add main function to example docs
    GitOrigin-RevId: cb6d5899f76a31e62f78a80700fd156636d294b2

--- docs/developer/advanced/snippets/send-from-address/send.ts
+++ docs/developer/advanced/snippets/send-from-address/send.ts
@@ -1,24 +1,28 @@
 import { MailSender, privateMessagingKeyFromHex } from '@mailchain/sdk/internal';
 
-// securely supply private messaging key hex bytes
-const recoveredPrivateMessagingKey = privateMessagingKeyFromHex('...');
+async function main() {
+	// securely supply private messaging key hex bytes
+	const recoveredPrivateMessagingKey = privateMessagingKeyFromHex('...');
 
-const mailSender = MailSender.fromSenderMessagingKey(recoveredPrivateMessagingKey);
+	const mailSender = MailSender.fromSenderMessagingKey(recoveredPrivateMessagingKey);
 
-const { data: sentMail, error: sendMailError } = await mailSender.sendMail({
-	// address related private messaging key
-	from: `yoursername@mailchain.com`,
-	// list of recipients (blockchain or mailchain addresses)
-	to: [`0xbb56FbD7A2caC3e4C17936027102344127b7a112@ethereum.mailchain.com`],
-	subject: 'My first message', // subject line
-	content: {
-		text: 'Hello Mailchain 👋', // plain text body
-		html: '<p>Hello Mailchain 👋</p>', // html body
-	},
-});
+	const { data: sentMail, error: sendMailError } = await mailSender.sendMail({
+		// address related private messaging key
+		from: `yoursername@mailchain.com`,
+		// list of recipients (blockchain or mailchain addresses)
+		to: [`0xbb56FbD7A2caC3e4C17936027102344127b7a112@ethereum.mailchain.com`],
+		subject: 'My first message', // subject line
+		content: {
+			text: 'Hello Mailchain 👋', // plain text body
+			html: '<p>Hello Mailchain 👋</p>', // html body
+		},
+	});
 
-if (sendMailError) {
-	throw sendMailError;
+	if (sendMailError) {
+		throw sendMailError;
+	}
+
+	console.log(`Message sent successfully: ${sentMail}`);
 }
 
-console.log(`Message sent successfully: ${sentMail}`);
+main();
